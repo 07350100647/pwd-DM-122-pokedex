5c4cef1d9e97a5e17d67d31a18c9983a8f8f453b
07350100647 <deniof@gee.inatel.br>
2022-10-29 17:41:01 -0300

    feat: add cards

--- pwa-pokedex/src/Home.jsx
+++ pwa-pokedex/src/Home.jsx
@@ -1,9 +1,16 @@
 import Nullstack from "nullstack";
-import Tag from "./components/Tag";
+import PokeCard from "./components/PokeCard";
 import NumberSort from "./icons/NumberSort";
 import PokeballIcon from "./icons/PokeballIcon";
 
 class Home extends Nullstack {
+  pokelist = [
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  ];
+
   prepare({ project, page }) {
     page.title = `${project.name}`;
     page.description = `${project.name} was made with Nullstack`;
@@ -21,10 +28,34 @@ class Home extends Nullstack {
     );
   }
 
+  renderSearchBar() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search pokemon"
+          class="w-full rounded-lg border border-gray-300 p-2"
+        />
+      </div>
+    );
+  }
+
+  renderPokeList() {
+    return (
+      <div class="flex gap-3">
+        {this.pokelist.map((pokeData) => (
+          <PokeCard pokeData={pokeData} />
+        ))}
+      </div>
+    );
+  }
+
   render() {
     return (
-      <section class="mx-auto min-h-screen w-full max-w-3xl px-4 py-5">
+      <section class="mx-auto flex min-h-screen w-full max-w-3xl flex-col gap-3 px-4 py-5">
         <Header />
+        <SearchBar />
+        <PokeList />
       </section>
     );
   }
