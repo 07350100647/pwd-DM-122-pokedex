4d49ae2db9c89dc08616ac86283e9bfef6bac35a
07350100647 <deniof@gee.inatel.br>
2022-10-29 19:14:52 -0300

    feat: add preload

--- pwa-pokedex/src/components/PokeCard.jsx
+++ pwa-pokedex/src/components/PokeCard.jsx
@@ -2,7 +2,12 @@ export default function PokeCard({ pokeData }) {
   return (
     <figure class="border-grass flex h-36 w-28 flex-col overflow-hidden rounded-lg border">
       <span class="w-full p-2 text-right text-xs">#001</span>
-      <img src={pokeData} alt="" class="aspect-square w-20 self-center" />
+      <img
+        src={pokeData}
+        alt=""
+        class="aspect-square w-20 self-center"
+        loading="lazy"
+      />
       <figcaption class="bg-grass mt-auto text-center capitalize text-white">
         Bulbasaur
       </figcaption>
