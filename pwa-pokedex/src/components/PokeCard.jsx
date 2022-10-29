81153c2ac823475a86171ef0234708aa84bb625c
07350100647 <deniof@gee.inatel.br>
2022-10-29 18:35:58 -0300

    feat: add 151 pokemon

--- pwa-pokedex/src/components/PokeCard.jsx
+++ pwa-pokedex/src/components/PokeCard.jsx
@@ -1,9 +1,11 @@
 export default function PokeCard({ pokeData }) {
   return (
-    <figure>
-      <span>#001</span>
-      <img src={pokeData} alt="" />
-      <figcaption>Bulbasaur</figcaption>
+    <figure class="border-grass flex h-36 w-28 flex-col overflow-hidden rounded-lg border">
+      <span class="w-full p-2 text-right text-xs">#001</span>
+      <img src={pokeData} alt="" class="aspect-square w-20 self-center" />
+      <figcaption class="bg-grass mt-auto text-center capitalize text-white">
+        Bulbasaur
+      </figcaption>
     </figure>
   );
 }
